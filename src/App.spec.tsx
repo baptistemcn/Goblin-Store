27ee7bfdf6ef27e23835453603bc7a2a7e816862
Baptiste Marcon <baptistemcn@icloud.com>
2021-10-24 00:27:54 +0200

    test: render Home component

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,17 +1,31 @@
-import React from "react"
-import { App } from "./App"
-import { createMemoryHistory } from 'history'
-import { render } from "@testing-library/react"
-import { Router } from "react-router-dom"
+import React from "react";
+import { App } from "./App";
+import { createMemoryHistory } from "history";
+import { render } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import { Home } from "./Home";
+
+jest.mock("./Home", () => ({ Home: () => <div>Home</div> }));
 
 describe("App", () => {
   it("renders successfully", () => {
-    const history = createMemoryHistory()
+    const history = createMemoryHistory();
     const { container } = render(
       <Router history={history}>
         <App />
       </Router>
-    )
-    expect(container.innerHTML).toMatch('Goblin Store')
-  })
-})
+    );
+    expect(container.innerHTML).toMatch("Goblin Store");
+  });
+
+  it("renders Home component on root route", () => {
+    const history = createMemoryHistory();
+    history.push("/");
+    const { container } = render(
+      <Router history={history}>
+        <Home />
+      </Router>
+    );
+    expect(container.innerHTML).toMatch("Home");
+  });
+});
