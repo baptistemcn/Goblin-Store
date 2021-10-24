8550c4c6724045ac9228e2321ae55b2732a8682d
Baptiste Marcon <baptistemcn@icloud.com>
2021-10-24 23:54:26 +0200

    test: fix test with new inputRef

--- src/Checkout/FormField.spec.tsx
+++ src/Checkout/FormField.spec.tsx
@@ -1,20 +1,19 @@
 import React from "react";
-import { fireEvent, render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { FormField } from "./FormField";
 
 describe("FormField", () => {
   it("renders correctly", () => {
     const { getByLabelText } = render(
-      <FormField label="Foo label" name="foo" />
+      <FormField label="Foo label" name="foo" inputRef={null} />
     );
-
     const input = getByLabelText("Foo label:");
     expect(input).toBeInTheDocument();
     expect(input).not.toHaveClass("is-error");
     expect(input).toHaveAttribute("name", "foo");
   });
 
-  describe("with errors", () => {
+  describe("with error", () => {
     it("renders error message", () => {
       const { getByText } = render(
         <FormField
@@ -23,7 +22,6 @@ describe("FormField", () => {
           errors={{ message: "Example error" }}
         />
       );
-
       expect(getByText("Error: Example error")).toBeInTheDocument();
     });
   });
@@ -40,7 +38,6 @@ describe("FormField", () => {
       );
 
       const input = getByLabelText("Foo label:") as HTMLInputElement;
-
       fireEvent.change(input, { target: { value: "test" } });
 
       expect(input.value).toEqual("TEST");
